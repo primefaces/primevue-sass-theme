f423a6c0f53122e570f6fca3c93507b506dc4a4d
mertsincan <sincan.mert@gmail.com>
2023-07-17 13:04:21 +0100

    Component: Avatar

---
+++ themes/primeone/tokens/misc/avatar.js
@@ -0,0 +1,15 @@
+export default {
+    root: {
+        color: '#495057',
+        background: '#dee2e6'
+    },
+    parents: {
+        inGroup: {
+            border: {
+                width: '2px',
+                style: 'solid',
+                color: '#ffffff'
+            }
+        }
+    }
+};
