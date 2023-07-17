f03011916fc7103e5314590a084fc31fc1fb7360
mertsincan <sincan.mert@gmail.com>
2023-07-17 14:03:46 +0100

    Component: Badge and Chip

---
+++ themes/primeone/tokens/misc/badge.js
@@ -0,0 +1,35 @@
+export default {
+    root: {
+        font: {
+            size: '0.75rem',
+            weight: 700
+        },
+        minWidth: '1.5rem',
+        height: '1.5rem',
+        lineHeight: '1.5rem',
+        color: 'var(--p-primary-text-color)',
+        background: 'var(--p-primary-color)',
+        variants: {
+            secondary: {
+                color: '#ffffff',
+                background: '#64748b'
+            },
+            success: {
+                color: '#ffffff',
+                background: '#22c55e'
+            },
+            info: {
+                color: '#ffffff',
+                background: '#3b82f6'
+            },
+            warning: {
+                color: '#ffffff',
+                background: '#f59e0b'
+            },
+            danger: {
+                color: '#ffffff',
+                background: '#ef4444'
+            }
+        }
+    }
+};
---
+++ themes/primeone/tokens/misc/chip.js
@@ -0,0 +1,29 @@
+export default {
+    root: {
+        color: '#495057',
+        background: '#dee2e6',
+        border: {
+            width: 'initial',
+            style: 'initial',
+            color: 'initial',
+            radius: '16px'
+        },
+        padding: {
+            top: 0,
+            right: '0.75rem',
+            bottom: 0,
+            left: '0.75rem'
+        }
+    },
+    removeIcon: {
+        states: {
+            outline: {
+                color: 'var(--p-primary-light-color)',
+                style: 'none',
+                width: 0,
+                offset: 0
+            },
+            boxShadow: '0 0 0 0.2rem var(--p-primary-light-color)'
+        }
+    }
+};
