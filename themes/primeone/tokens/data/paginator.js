8b533c03e767cd9ed65bff8a7c37e8449d71006e
mertsincan <sincan.mert@gmail.com>
2023-08-22 17:43:32 +0100

    Component: Paginator

---
+++ themes/primeone/tokens/data/paginator.js
@@ -0,0 +1,113 @@
+export default {
+    root: {
+        color: '#6c757d',
+        background: '#ffffff',
+        border: {
+            width: '0',
+            style: 'solid',
+            color: '#e9ecef'
+        },
+        padding: {
+            top: '0.5rem',
+            right: '1rem',
+            bottom: '0.5rem',
+            left: '1rem'
+        }
+    },
+    pageButton: {
+        height: '3rem',
+        minWidth: '3rem',
+        color: '#6c757d',
+        background: 'transparent',
+        border: {
+            width: '0',
+            style: 'none',
+            color: 'initial',
+            radius: '50%'
+        },
+        margin: {
+            top: '0.143rem',
+            right: '0.143rem',
+            bottom: '0.143rem',
+            left: '0.143rem'
+        },
+        states: {
+            hover: {
+                color: '#343a40',
+                background: '#e9ecef',
+                border: {
+                    width: 'inherit',
+                    style: 'inherit',
+                    color: 'transparent'
+                }
+            }
+        }
+    },
+    pageDropdown: {
+        height: '3rem'
+    },
+    jumpToPageInput: {
+        maxWidth: '3rem'
+    },
+    current: {
+        height: '3rem',
+        minWidth: '3rem',
+        color: '#6c757d',
+        background: 'transparent',
+        border: {
+            width: '0',
+            style: 'none',
+            color: 'initial'
+        },
+        padding: {
+            top: '0',
+            right: 'var(--p-inline-spacing)',
+            bottom: '0',
+            left: 'var(--p-inline-spacing)'
+        },
+        margin: {
+            top: '0.143rem',
+            right: '0.143rem',
+            bottom: '0.143rem',
+            left: '0.143rem'
+        }
+    },
+    pageButton: {
+        height: '3rem',
+        minWidth: '3rem',
+        color: '#6c757d',
+        background: 'transparent',
+        border: {
+            width: '0',
+            style: 'none',
+            color: 'initial',
+            radius: '50%'
+        },
+        margin: {
+            top: '0.143rem',
+            right: '0.143rem',
+            bottom: '0.143rem',
+            left: '0.143rem'
+        },
+        states: {
+            active: {
+                color: 'var(--p-primary-darker-color)',
+                background: '#eff6ff',
+                border: {
+                    width: 'inherit',
+                    style: 'inherit',
+                    color: '#eff6ff'
+                }
+            },
+            hover: {
+                color: '#343a40',
+                background: '#e9ecef',
+                border: {
+                    width: 'inherit',
+                    style: 'inherit',
+                    color: 'transparent'
+                }
+            }
+        }
+    }
+};
