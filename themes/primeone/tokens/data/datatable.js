f5f8d91b8f6eb3957a3aa919fea111998f5e9f6d
mertsincan <sincan.mert@gmail.com>
2023-08-23 15:42:14 +0100

    Refactor DataTable

--- themes/primeone/tokens/data/datatable.js
+++ themes/primeone/tokens/data/datatable.js
@@ -245,6 +245,208 @@ export default {
     loadingIcon: {
         width: '2rem'
     },
+    filterMenuButton: {
+        width: '2rem',
+        height: '2rem',
+        color: '#6c757d',
+        background: 'transparent',
+        border: {
+            width: 0,
+            style: 'none',
+            color: 'initial',
+            radius: '50%'
+        },
+        states: {
+            hover: {
+                color: '#343a40',
+                background: '#e9ecef',
+                border: {
+                    width: 'inherit',
+                    style: 'inherit',
+                    color: 'transparent'
+                }
+            },
+            focus: {
+                outline: {
+                    color: 'var(--p-primary-light-color)',
+                    style: 'none',
+                    width: 0,
+                    offset: 0
+                },
+                boxShadow: '0 0 0 0.2rem var(--p-primary-light-color)'
+            }
+        },
+        variants: {
+            open: {
+                color: '#343a40',
+                background: '#e9ecef'
+            },
+            active: {
+                color: 'var(--p-primary-darker-color)',
+                background: '#eff6ff'
+            }
+        }
+    },
+    headerFilterClearButton: {
+        width: '2rem',
+        height: '2rem',
+        color: '#6c757d',
+        background: 'transparent',
+        border: {
+            width: 0,
+            style: 'none',
+            color: 'initial',
+            radius: '50%'
+        },
+        states: {
+            hover: {
+                color: '#343a40',
+                background: '#e9ecef',
+                border: {
+                    width: 'inherit',
+                    style: 'inherit',
+                    color: 'transparent'
+                }
+            },
+            focus: {
+                outline: {
+                    color: 'var(--p-primary-light-color)',
+                    style: 'none',
+                    width: 0,
+                    offset: 0
+                },
+                boxShadow: '0 0 0 0.2rem var(--p-primary-light-color)'
+            }
+        }
+    },
+    filterOverlay: {
+        minWidth: '12.5rem',
+        color: '#495057',
+        background: '#ffffff',
+        border: {
+            width: '0',
+            style: 'none',
+            color: 'initial',
+            radius: 'var(--p-border-radius)'
+        },
+        boxShadow: '0 2px 12px 0 rgba(0, 0, 0, 0.1)',
+        variants: {
+            menu: {
+                filterOperator: {
+                    color: '#343a40',
+                    background: '#f8f9fa',
+                    padding: {
+                        top: '0.75rem',
+                        right: '1.25rem',
+                        bottom: '0.75rem',
+                        left: '1.25rem'
+                    },
+                    margin: {
+                        top: '0',
+                        right: '0',
+                        bottom: '0',
+                        left: '0'
+                    },
+                    border: {
+                        width: '0 0 1px 0',
+                        style: 'solid',
+                        color: '#dee2e6'
+                    }
+                },
+                filterConstraint: {
+                    border: {
+                        width: '0 0 1px 0',
+                        style: 'solid',
+                        color: '#dee2e6'
+                    },
+                    padding: {
+                        top: '1.25rem',
+                        right: '1.25rem',
+                        bottom: '1.25rem',
+                        left: '1.25rem'
+                    }
+                },
+                filterAddRule: {
+                    padding: {
+                        top: '0.75rem',
+                        right: '1.25rem',
+                        bottom: '0.75rem',
+                        left: '1.25rem'
+                    }
+                },
+                filterButtonbar: {
+                    padding: {
+                        top: '1.25rem',
+                        right: '1.25rem',
+                        bottom: '1.25rem',
+                        left: '1.25rem'
+                    }
+                }
+            }
+        }
+    },
+    filterRowItems: {
+        padding: {
+            top: '0.75rem',
+            right: '0',
+            bottom: '0.75rem',
+            left: '0'
+        }
+    },
+    filterRowItem: {
+        color: '#495057',
+        background: 'transparent',
+        border: {
+            width: '0',
+            style: 'none',
+            color: 'initial',
+            radius: '0'
+        },
+        margin: {
+            top: '0',
+            right: '0',
+            bottom: '0',
+            left: '0'
+        },
+        padding: {
+            top: '0.75rem',
+            right: '1.25rem',
+            bottom: '0.75rem',
+            left: '1.25rem'
+        },
+        states: {
+            active: {
+                color: 'var(--p-primary-darker-color)',
+                background: '#eff6ff'
+            },
+            hover: {
+                color: '#495057',
+                background: '#e9ecef'
+            },
+            focus: {
+                outline: {
+                    color: 'initial',
+                    style: 'none',
+                    width: '0',
+                    offset: '0'
+                },
+                boxShadow: 'inset 0 0 0 0.15rem var(--p-primary-light-color)'
+            }
+        }
+    },
+    filterSeparator: {
+        border: {
+            width: '1px 0 0 0',
+            style: 'solid',
+            color: '#dee2e6'
+        },
+        margin: {
+            top: '0.25rem',
+            right: '0',
+            bottom: '0.25rem',
+            left: '0'
+        }
+    },
     variants: {
         hoverableRows: {
             bodyRow: {
